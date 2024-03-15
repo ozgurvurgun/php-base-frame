69d01e651da2456cfd19e5cad4c0b1ddffcb0c8b
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2024-03-15 03:09:33 +0300

    env.php edited

--- env.php
+++ env.php
@@ -7,9 +7,9 @@
    when using base URL in static files (HTML, JavaScript, etc.)
  */
 
-$BASE_URL = "http://localhost/php-base-frame";
-$DB_HOST = "localhost";
-$DB_CHAREST = "utf8mb4";
-$DB_NAME = "";
-$DB_USER = "";
-$DB_PASSWORD = "";
+ $BASE_URL = "http://localhost/php-base-frame";
+ $DB_HOST = "localhost";
+ $DB_CHAREST = "utf8mb4";
+ $DB_NAME = "php_base_frame";
+ $DB_USER = "root";
+ $DB_PASSWORD = "";
